88b191b51dea7ebfb6edc00ca365e963f1f9ed3c
Daniel <darkdanielr@gmail.com>
2022-10-12 23:51:53 -0300

    arrumando prisma e pack

--- Build/repositories/implementations/MySQLFiguresRepository.js
+++ Build/repositories/implementations/MySQLFiguresRepository.js
@@ -2,9 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MySQLUserRepository = void 0;
 const client_1 = require("@prisma/client");
+const process_1 = require("process");
 class MySQLUserRepository {
     constructor() {
-        this.prisma = new client_1.PrismaClient();
+        this.prisma = new client_1.PrismaClient({
+            datasources: {
+                db: {
+                    url: process_1.env.DATABASE_URL,
+                },
+            },
+        });
     }
     async findByID(id) {
         const figure = await this.prisma.figures.findUnique({
